414997b8afa20bf43d1791d02f1ccffa9fb65b4d
sunhongchao1 <sunhongchao@jd.com>
2021-03-09 22:40:11 +0800

    update

--- solutions/mrc/multi_choice/bert.py
+++ solutions/mrc/multi_choice/bert.py
@@ -10,7 +10,8 @@ class BERT(nn.Module):
         self.bert = bert
         self.dropout = nn.Dropout(opt.dropout)
         self.softmax = nn.Softmax(dim=1)
-        self.dense1 = nn.Linear(opt.bert_dim, 1)
+        self.softmax1 = nn.Softmax(dim=1)
+        self.dense1 = nn.Linear(opt.bert_dim, 2)
         #self.dense2 = nn.Linear(opt.bert_dim, 4)
 
     def forward(self, inputs):
@@ -29,11 +30,29 @@ class BERT(nn.Module):
         #print('c', c_pooled_output)
         #print('d', d_pooled_output)
 
-        a_ = self.dense1(self.dropout(a_pooled_output))
-        b_ = self.dense1(self.dropout(b_pooled_output))
-        c_ = self.dense1(self.dropout(c_pooled_output))
-        d_ = self.dense1(self.dropout(d_pooled_output))
+        a_ = self.softmax1(self.dense1(self.dropout(a_pooled_output)))
+        b_ = self.softmax1(self.dense1(self.dropout(b_pooled_output)))
+        c_ = self.softmax1(self.dense1(self.dropout(c_pooled_output)))
+        d_ = self.softmax1(self.dense1(self.dropout(d_pooled_output)))
 
-        cat = torch.cat((a_, b_, c_, d_), 1)
+        #print('a', a_)
+        #print('b', b_)
+        #print('c', c_)
+        #print('d', d_)
+        
+        indices = torch.LongTensor([0]).cuda()
+        index_select_a = torch.index_select(a_, 1, indices).cuda()
+        #print('select a ', index_select_a)
+        index_select_b = torch.index_select(b_, 1, indices).cuda()
+        #print('select b ', index_select_b)
+        index_select_c = torch.index_select(c_, 1, indices).cuda()
+        #print('select c ', index_select_c)
+        index_select_d = torch.index_select(d_, 1, indices).cuda()
+        #print('select d ', index_select_d)
+
+        #cat = torch.cat((a_, b_, c_, d_), 1)
+        cat = torch.cat((index_select_a, index_select_b, index_select_c, index_select_d), 1)
+        #print('cat', cat)
         softmax = self.softmax(cat)
-        return softmax
+        #print('softmax', softmax)
+        return cat, softmax
