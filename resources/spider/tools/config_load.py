05cc8a35476b2311d0bf9f6be6ec31fd8d854e2e
apollo2mars <apollo2mars@gmail.com>
2022-10-09 22:20:41 +0800

    update

---
+++ resources/spider/tools/config_load.py
@@ -0,0 +1,71 @@
+import configparser
+
+__all__ = ['get_config_loader']
+
+
+# --- Infrastructure --
+class ConfigLoader:
+    """
+    Attributes:
+
+    Methods: read(): read conf file
+    """
+    def __init__(self):
+        self._config = configparser.ConfigParser()
+
+    def read(self, file_path):
+        """
+        Args:
+            file_path: string
+        Returns:
+            ret: int, 0 success, else fail
+        """
+        ret = self._config.read(file_path)
+        if file_path in ret:
+            return 0
+        else:
+            return -1
+
+    def get(self, section, option):
+        """
+        Args:
+            section: string
+            option: string
+        Returns:
+            value: string
+        """
+        return self._config.get(section, option)
+
+    def get_int(self, section, option):
+        """
+        Args:
+            section: string
+            option: string
+        Returns:
+            value: string
+        """
+        return self._config.get(section, option)
+
+    def items(self, section):
+        """
+        Return a list of (name, value) pairs for each option 
+        in the given section.
+        
+        Args:
+            section: string
+        Returns:
+            items: list of (name, value)
+        """
+        return self._config.items(section)
+# --- End Infrastructure --
+
+
+# --- Sigleton ---
+_config_loader = ConfigLoader()
+# --- End Sigleton ---
+
+
+# --- API ---
+def get_config_loader():
+    return _config_loader
+# --- End API ---
