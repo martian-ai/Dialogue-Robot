1334a98a5c1542805e5ec801e1ce1fbc421e0d6c
apollo2mars <apollo2mars@gmail.com>
2022-10-30 22:53:08 +0800

    update

--- docs/python/sorted.py
+++
@@ -1,7 +0,0 @@
-# sort 和 sorted https://www.runoob.com/python/python-func-sorted.html
-# sort 处理list， 返回null
-# sorted 处理迭代器，返回排序后的数据结构
-
-
-    # input_lines = sorted(list(enumerate(input_lines)), key=lambda x: -len(x[1]))
-    # 按照长度排序
--- docs/python/zip.py
+++
@@ -1,3 +0,0 @@
-# zip 和 zip(*) https://blog.csdn.net/qq_27921205/article/details/104045976
-# zip(*sorted(zip()))
-# reverse=False默认为升序，当reverse=True表示降序
